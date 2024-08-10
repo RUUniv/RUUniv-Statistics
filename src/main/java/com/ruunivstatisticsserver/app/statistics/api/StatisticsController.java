eba0633fc53ae5bfbb929811055263e9543bc0c8
bandalgomsu <q63530@naver.com>
2024-08-10 14:27:13 +0900

    feat : swagger 기본설정

--- src/main/java/com/ruunivstatisticsserver/app/statistics/api/StatisticsController.java
+++ src/main/java/com/ruunivstatisticsserver/app/statistics/api/StatisticsController.java
@@ -1,10 +1,7 @@
 package com.ruunivstatisticsserver.app.statistics.api;
 
-import com.ruunivstatisticsserver.app.statistics.entity.Statistics;
-import com.ruunivstatisticsserver.app.statistics.repository.StatisticsRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,14 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 @RequestMapping("/api/v1/statistics")
 public class StatisticsController {
-    private final StatisticsRepository statisticsRepository;
 
-    @GetMapping
-    public void test(){
-        Statistics test = Statistics.builder()
-                .apiKey("asd")
-                .build();
 
-        statisticsRepository.save(test);
-    }
 }
