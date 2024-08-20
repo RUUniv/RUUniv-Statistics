31d107d5cbbd1f585f7951a74b7f0f472f6deeb2
bandalgomsu <q63530@naver.com>
2024-08-20 21:46:54 +0900

    infra : Mongo 설정 변경

--- src/main/java/com/ruunivstatisticsserver/app/statistics/api/StatisticsController.java
+++ src/main/java/com/ruunivstatisticsserver/app/statistics/api/StatisticsController.java
@@ -1,7 +1,9 @@
 package com.ruunivstatisticsserver.app.statistics.api;
 
+import com.ruunivstatisticsserver.app.statistics.service.StatisticsService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +12,10 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 @RequestMapping("/api/v1/statistics")
 public class StatisticsController {
+    private final StatisticsService statisticsService;
+
+    @GetMapping
+    public void test() {
+        statisticsService.collectionStatistics("asd", "GET", 500, "asd");
+    }
 }
