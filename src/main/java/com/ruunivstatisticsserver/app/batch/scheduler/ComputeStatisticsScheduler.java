735c6a8e6f4a1191a45f274dcf370d776028993c
bandalgomsu <q63530@naver.com>
2024-09-02 14:50:17 +0900

    feat : Compute Statistics Job Scheduler 구현

---
+++ src/main/java/com/ruunivstatisticsserver/app/batch/scheduler/ComputeStatisticsScheduler.java
@@ -0,0 +1,25 @@
+package com.ruunivstatisticsserver.app.batch.scheduler;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersInvalidException;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class ComputeStatisticsScheduler {
+    private final JobLauncher jobLauncher;
+    private final Job job;
+
+    @Scheduled(cron = "* 10 0 * * *")
+    public void computeStatisticsDetailSchedule()
+            throws JobInstanceAlreadyCompleteException, JobExecutionAlreadyRunningException, JobParametersInvalidException, JobRestartException {
+        jobLauncher.run(job, new JobParameters());
+    }
+}
--- src/main/java/com/ruunivstatisticsserver/app/statistics/api/StatisticsController.java
+++ src/main/java/com/ruunivstatisticsserver/app/statistics/api/StatisticsController.java
@@ -1,15 +1,10 @@
 package com.ruunivstatisticsserver.app.statistics.api;
 
 import com.ruunivstatisticsserver.app.statistics.dto.StatisticsResponse.StatisticsMonthInfo;
-import com.ruunivstatisticsserver.app.statistics.entity.Statistics;
 import com.ruunivstatisticsserver.app.statistics.service.StatisticsService;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import java.time.LocalDate;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.query.Criteria;
-import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,7 +19,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/statistics")
 public class StatisticsController {
     private final StatisticsService statisticsService;
-    private final MongoTemplate mongoTemplate;
 
     @GetMapping("/{apiKey}")
     public ResponseEntity<StatisticsMonthInfo> getStatisticsMonthInfo(@PathVariable String apiKey,
@@ -32,19 +26,5 @@ public class StatisticsController {
         StatisticsMonthInfo response = statisticsService.getStatisticsInfoByMonth(apiKey, month);
 
         return ResponseEntity.ok(response);
-    }
-
-    @GetMapping("/test")
-    public void getStatisticsMonthInfo() {
-        LocalDate s = LocalDate.now().minusMonths(1);
-        LocalDate e = LocalDate.now();
-
-        Query query = new Query();
-        query.addCriteria(Criteria.where("createdDate").gte(s).lt(e).
-                and("apiKey").is("asdf")
-        );
-
-        mongoTemplate.find(query, Statistics.class);
-
     }
 }
